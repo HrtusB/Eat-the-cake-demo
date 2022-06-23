f58a562220458a42850cfdcd19bf135081a0f301
HrtusB <b.hrtus@seznam.cz>
2022-06-23 16:07:50 +0200

    Small changes

--- game/cake/eat-cake.js
+++ game/cake/eat-cake.js
@@ -1,6 +1,7 @@
 import { end } from "../end.js";
 import { createCake } from "./create-cake.js";
 
+    //counting how many cakes were eaten
 var counter = {
     count: 0
 }
@@ -14,12 +15,14 @@ export function eatCake() {
     let cx = parseInt(cake.getPropertyValue('left'));
     let cy = parseInt(cake.getPropertyValue('top'));
 
-
+    //compare position of cake and player
     if (px <= cx && (px + 40) >= (cx + 20) && py <= cy && (py + 40) >= (cy + 20)) {
        console.log("nom nom nom nom");
        createCake();
        counter.count++;
     }    
+
+    //its just a demo so make it simple and max 10 cakes
     if (counter.count === 10) {
         end();
     }
